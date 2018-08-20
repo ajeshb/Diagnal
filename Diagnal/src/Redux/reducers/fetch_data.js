0b13132ddd1df189e391efb1063331a20037ff7b
ajeshb <ajesh.16.09@gmail.com>
2018-08-20 09:30:07 +0530

    video list append implemented

--- Diagnal/src/Redux/reducers/fetch_data.js
+++ Diagnal/src/Redux/reducers/fetch_data.js
@@ -1,24 +1,22 @@
+const fetchData = (state = { videoList: [] }, action) => {
+  switch (action.type) {
+    case 'FETCH_DATA':
+      console.log('action', action)
+      const response = require(`./../../Data/CONTENTLISTINGPAGE-PAGE${action.param}.json`)
+      const pageData = response.page
+      const newVideos = pageData['content-items']['content']
+      const newstate = {
+        ...state,
+        videoList: [...state.videoList, ...newVideos],
+        heading: pageData.title,
+        totalVideos: pageData['total-content-items']
+      }
+      console.log('state', state)
+      console.log('newstate', newstate)
+      return newstate
+    default:
+      return state
+  }
+}
 
-const fetchData = (state = {}, action) => {
-    switch (action.type) {
-        case 'FETCH_DATA':
-            console.log('action',action);
-
-            const response = require(`./../../Data/CONTENTLISTINGPAGE-PAGE${action.param}.json`)
-            const pageData = response.page
-            const newVideos = pageData['content-items']['content']
-            const newstate = {
-                ...state,
-                videoList: [...newVideos],
-                heading: pageData.title,
-                totalVideos: pageData['total-content-items']
-            };
-            console.log('state', state);
-            console.log('newstate', newstate);
-            return newstate;
-        default: 
-            return state;
-    }
-};
-
-export default fetchData;
+export default fetchData
